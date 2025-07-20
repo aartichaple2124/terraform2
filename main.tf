8ef1ac939858c5069f278c19a988d6dd83946165
LP269 <aartichaple2124@gmail.com>
2025-07-20 23:10:01 +0530

    Frontend shell

--- main.tf
+++ main.tf
@@ -13,5 +13,54 @@ variable "component" {
       name    = "frontend"
       vm_size = "Standard_B4ms"
     }
+    mongodb = {
+      name = "mongodb"
+      vm_size = "Standard_B4ms"
+    }
+
+    catalogue = {
+      name = "catalogue"
+      vm_size = "Standard_B4ms"
+    }
+
+    user = {
+      name = "user"
+      vm_size = "Standard_B4ms"
+    }
+
+    cart = {
+      name = "cart"
+      vm_size = "Standard B2s"
+    }
+
+    mysql = {
+      name = "mysql"
+      vm_size = "Standard_B1ms"
+    }
+
+    shipping = {
+      name = "shipping"
+      vm_size = "Standard_B1ms"
+    }
+
+    payment = {
+      name = "payment"
+      vm_size = "Standard_B1ms"
+    }
+
+    redis = {
+      name = "redis"
+      vm_size = "Standard_B1ms"
+    }
+
+    dispatch = {
+      name = "dispatch"
+      vm_size = "Standard_B1ms"
+    }
+
+    rabbitmq = {
+      name = "rabbitmq"
+      vm_size = "Standard_B1ms"
+    }
   }
 }
